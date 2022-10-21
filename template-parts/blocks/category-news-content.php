b8131438dea12c9ebde3ad43ad6fd1175f0a7424
Roni Viitanen <roni.viitanen@astehelsinki.fi>
2022-10-21 10:58:33 +0300

    Revert "RSS Feed updates"
    This reverts commit 003316fc0c55b2f6b19ef4f465707026c925651c.

--- template-parts/blocks/category-news-content.php
+++ template-parts/blocks/category-news-content.php
@@ -1,28 +1,21 @@
 <?php
 $ingressi = get_the_excerpt($postID);
-
-if (get_field('rss_url')) {
-  $link = get_field('rss_url');
-} else {
-  $link = get_the_permalink($post->ID);
-}
-
 ?>
 
-<a href="<?= $link ?>" class="column is-4 is-12-mobile">
-  <div class="element">
-    <figure class="image is-3by2">
-      <?php if (has_post_thumbnail()) : ?>
-       <img class="is-square" src="<?= get_the_post_thumbnail_url( $post->ID, 'large' ); ?>" alt="">
-     <?php else : ?>
-      <img class="is-square" src="https://kestava.helsinki/content/uploads/2021/05/pictureplaceholder3_2_M.png" alt="">
-    <?php endif; ?>
-  </figure>
-  <div class="news-content">
-    <p class="date"><?= get_the_date('d.m.Y'); ?></p>
-    <h3 class="title is-4 is-medium"><?php the_title(); ?></h3>
-    <p class="excerpt"><?php echo wp_trim_words($ingressi, 11, '...' ); ?></p></div>
-    <div class="hds-icon hds-icon--size-l hds-icon--arrow-right"></div>
-  </div>
+<a href="<?php the_permalink(); ?>" class="column is-4 is-12-mobile">
+<div class="element">
+<figure class="image is-3by2">
+  <?php if (has_post_thumbnail()) : ?>
+   <img class="is-square" src="<?= get_the_post_thumbnail_url( $post->ID, 'large' ); ?>" alt="">
+ <?php else : ?>
+  <img class="is-square" src="https://kestava.helsinki/content/uploads/2021/05/pictureplaceholder3_2_M.png" alt="">
+<?php endif; ?>
+</figure>
+<div class="news-content">
+<p class="date"><?= get_the_date('d.m.Y'); ?></p>
+<h3 class="title is-4 is-medium"><?php the_title(); ?></h3>
+<p class="excerpt"><?php echo wp_trim_words($ingressi, 11, '...' ); ?></p></div>
+<div class="hds-icon hds-icon--size-l hds-icon--arrow-right"></div>
+</div>
 </a>
 
