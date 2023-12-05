4d51de7235dc77f27863adb144cb82d23edb1db8
Antti Koskinen <koskinenaa@users.noreply.github.com>
2023-12-05 21:26:39 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -9,6 +9,7 @@
  * @package Bulmascores
  */
 
+$current_language = pll_current_language();
 ?>
 
 </div><!-- #content -->
@@ -16,64 +17,51 @@
 <footer class="footer wave-bottom bc-valkoinen">
 	<div class="container">
 		<div class="content">
-    <?php
-      if( have_rows('footer', 'options') ):
-        while( have_rows('footer', 'options') ) : the_row();
+			<?php
+				if ( have_rows('footer', 'options') ) {
+					while ( have_rows('footer', 'options') ) {
+						the_row();
 
-      $footer_fi = get_sub_field('footer_fi');
-      $footer_en = get_sub_field('footer_en');
-      $footer_sv = get_sub_field('footer_sv');
-      $lang = pll_current_language();
+						if ( $current_language === 'fi' ) {
+							echo get_sub_field( 'footer_fi' );
+						} else if ( $current_language === 'en' ) {
+							echo get_sub_field( 'footer_en');
+						} else {
+							echo get_sub_field( 'footer_sv' );
+						}
+					}
+				}
+			?>
+	    </div>
 
-              if ($lang == 'fi') {
-                echo $footer_fi;
-              } elseif($lang == 'en') {
-                echo $footer_en;
-              } else {
-                echo $footer_sv;
-             } ?>
+	    <?php if ( have_rows('footer_links', 'options') ) : ?>
+			<div class="columns is-vcentered">
+				<div class="container">
+					<figure class="image">
+						<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/HELSINKI_Tunnus_VALKOINEN.png" alt="Footer logo">
+		            </figure>
 
-            <? endwhile;
-          endif;?>
-    </div>
+					<div class="footer-info">
+						<span>&copy; <?php pll_e('Helsingin kaupunki'); ?>, <?php echo date("Y"); ?></span>
 
-    <?php
+						<?php
+							while ( have_rows('footer_links', 'options') ) {
+								the_row();
 
-    if( have_rows('footer_links', 'options') ): ?>
-      <div class="columns is-vcentered">
-        <div class="container">
-          <figure class="image">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/HELSINKI_Tunnus_VALKOINEN.png" alt="Footer logo">
-          </figure>
-          <div class="footer-info">
-            <span>&copy; <?php pll_e('Helsingin kaupunki') ?>, <?php echo date("Y"); ?></span>
-
-            <?php
-            while( have_rows('footer_links', 'options') ) : the_row();
-              $title = get_sub_field('otsikko');
-              $title_en = get_sub_field('otsikko_en');
-              $title_sv = get_sub_field('otsikko_sv');
-
-              $link = get_sub_field('linkki');
-              $link_en = get_sub_field('linkki_en');
-              $link_sv = get_sub_field('linkki_sv');
-
-              $lang = pll_current_language();
-
-              if ($lang == 'fi') {
-                echo '<a href="'.$link.'">'.$title.'</a>';
-              } elseif($lang == 'en') {
-                echo '<a href="'.$link_en.'">'.$title_en.'</a>';
-              } else {
-               echo '<a href="'.$link_sv.'">'.$title_sv.'</a>';
-             }
-
-           endwhile;
-         endif; ?>
-       </div>
-     </div>
-   </div>
- </div>
+								if ( $current_language == 'fi' ) {
+									echo '<a href="' . get_sub_field('linkki') . '">' . get_sub_field('otsikko') . '</a>';
+								} else if ( $current_language == 'en' ) {
+									echo '<a href="' . get_sub_field('linkki_en') . '">' . get_sub_field('otsikko_en') . '</a>';
+								} else {
+									echo '<a href="' . get_sub_field('linkki_sv') . '">' . get_sub_field('otsikko_sv') . '</a>';
+								}
+							}
+						?>
+					</div>
+				</div>
+			</div>
+		<?php endif; ?>
+	</div>
 </footer>
 </div><!-- #page -->
 
