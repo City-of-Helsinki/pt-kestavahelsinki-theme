0b8fab8f0ba9f68e1fde796cb9ff6836325d8aeb
mariperasto <mari.perasto@astehelsinki.fi>
2025-02-03 16:04:03 +0200

    fix: the_field changed to get_field, this should fix iframe problem with wysiwyg editor

--- template-parts/blocks/sdg-list.php
+++ template-parts/blocks/sdg-list.php
@@ -34,136 +34,136 @@
         <h1><?php pll_e('SDG-1') ?></h1>
         <span class="circle-<?php echo get_field('circle_color')?>"></span>
       </div>
-      <div class="box-1" id="box1"><?php the_field('ei_koyhyytta1');?></div>
-      <div class="box-2" id="box1"><?php the_field('ei_koyhyytta2');?></div>
+      <div class="box-1" id="box1"><?php get_field('ei_koyhyytta1');?></div>
+      <div class="box-2" id="box1"><?php get_field('ei_koyhyytta2');?></div>
     </div>
     <div class="box">
       <div class="sdg-title">
         <h1><?php pll_e('SDG-2') ?></h1>
         <span class="circle-<?php echo get_field('circle_color_kopioi2')?>"></span>
       </div>
-      <div class="box-1" id="box2"><?php the_field('ei_nalkaa1');?></div>
-      <div class="box-2" id="box2"><?php the_field('ei_nalkaa2');?></div>
+      <div class="box-1" id="box2"><?php get_field('ei_nalkaa1');?></div>
+      <div class="box-2" id="box2"><?php get_field('ei_nalkaa2');?></div>
     </div>
     <div class="box">
       <div class="sdg-title">
         <h1><?php pll_e('SDG-3') ?></h1>
         <span class="circle-<?php echo get_field('circle_color_kopioi3')?>"></span>
       </div>
-      <div class="box-1" id="box3"><?php the_field('terveytta_ja_hyvinvointia1');?></div>
-      <div class="box-2" id="box3"><?php the_field('terveytta_ja_hyvinvointia2');?></div>
+      <div class="box-1" id="box3"><?php get_field('terveytta_ja_hyvinvointia1');?></div>
+      <div class="box-2" id="box3"><?php get_field('terveytta_ja_hyvinvointia2');?></div>
     </div>
     <div class="box">
       <div class="sdg-title">
         <h1><?php pll_e('SDG-4') ?></h1>
         <span class="circle-<?php echo get_field('circle_color_kopioi4')?>"></span>
       </div>
-      <div class="box-1" id="box4"><?php the_field('hyva_koulutus1');?></div>
-      <div class="box-2" id="box4"><?php the_field('hyva_koulutus2');?></div>
+      <div class="box-1" id="box4"><?php get_field('hyva_koulutus1');?></div>
+      <div class="box-2" id="box4"><?php get_field('hyva_koulutus2');?></div>
     </div>
     <div class="box">
       <div class="sdg-title">
         <h1><?php pll_e('SDG-5') ?></h1>
         <span class="circle-<?php echo get_field('circle_color_kopioi5')?>"></span>
       </div>
-      <div class="box-1" id="box5"><?php the_field('sukupuolten_tasaarvo1');?></div>
-      <div class="box-2" id="box5"><?php the_field('sukupuolten_tasaarvo2');?></div>
+      <div class="box-1" id="box5"><?php get_field('sukupuolten_tasaarvo1');?></div>
+      <div class="box-2" id="box5"><?php get_field('sukupuolten_tasaarvo2');?></div>
     </div>
     <div class="box">
       <div class="sdg-title">
         <h1><?php pll_e('SDG-6') ?></h1>
         <span class="circle-<?php echo get_field('circle_color_kopioi6')?>"></span>
       </div>
-      <div class="box-1" id="box6"><?php the_field('puhdas_vesi_ja_sanitaatio1');?></div>
-      <div class="box-2" id="box6"><?php the_field('puhdas_vesi_ja_sanitaatio2');?></div>
+      <div class="box-1" id="box6"><?php get_field('puhdas_vesi_ja_sanitaatio1');?></div>
+      <div class="box-2" id="box6"><?php get_field('puhdas_vesi_ja_sanitaatio2');?></div>
     </div>
     <div class="box">
       <div class="sdg-title">
         <h1><?php pll_e('SDG-7') ?></h1>
         <span class="circle-<?php echo get_field('circle_color_kopioi7')?>"></span>
       </div>
-      <div class="box-1" id="box7"><?php the_field('edullista_ja_puhdasta_energiaa1');?></div>
-      <div class="box-2" id="box7"><?php the_field('edullista_ja_puhdasta_energiaa2');?></div>
+      <div class="box-1" id="box7"><?php get_field('edullista_ja_puhdasta_energiaa1');?></div>
+      <div class="box-2" id="box7"><?php get_field('edullista_ja_puhdasta_energiaa2');?></div>
     </div>
     <div class="box">
       <div class="sdg-title">
         <h1><?php pll_e('SDG-8') ?></h1>
         <span class="circle-<?php echo get_field('circle_color_kopioi8')?>"></span>
       </div>
-      <div class="box-1" id="box8"><?php the_field('ihmisarvoista_tyota_ja_talouskasvua1');?></div>
-      <div class="box-2" id="box8"><?php the_field('ihmisarvoista_tyota_ja_talouskasvua2');?></div>
+      <div class="box-1" id="box8"><?php get_field('ihmisarvoista_tyota_ja_talouskasvua1');?></div>
+      <div class="box-2" id="box8"><?php get_field('ihmisarvoista_tyota_ja_talouskasvua2');?></div>
     </div>
     <div class="box">
       <div class="sdg-title">
         <h1><?php pll_e('SDG-9') ?></h1>
         <span class="circle-<?php echo get_field('circle_color_kopioi9')?>"></span>
       </div>
-      <div class="box-1" id="box9"><?php the_field('kestavaa_teollisuutta1');?></div>
-      <div class="box-2" id="box9"><?php the_field('kestavaa_teollisuutta2');?></div>
+      <div class="box-1" id="box9"><?php get_field('kestavaa_teollisuutta1');?></div>
+      <div class="box-2" id="box9"><?php get_field('kestavaa_teollisuutta2');?></div>
     </div>
     <div class="box">
       <div class="sdg-title">
         <h1><?php pll_e('SDG-10') ?></h1>
         <span class="circle-<?php echo get_field('circle_color_kopioi10')?>"></span>
       </div>
-      <div class="box-1" id="box10"><?php the_field('eriarvoisuuden_vahentaminen1');?></div>
-      <div class="box-2" id="box10"><?php the_field('eriarvoisuuden_vahentaminen2');?></div>
+      <div class="box-1" id="box10"><?php get_field('eriarvoisuuden_vahentaminen1');?></div>
+      <div class="box-2" id="box10"><?php get_field('eriarvoisuuden_vahentaminen2');?></div>
     </div>
     <div class="box">
       <div class="sdg-title">
         <h1><?php pll_e('SDG-11') ?></h1>
         <span class="circle-<?php echo get_field('circle_color_kopioi11')?>"></span>
       </div>
-      <div class="box-1" id="box11"><?php the_field('kestavat_kaupungit1');?></div>
-      <div class="box-2" id="box11"><?php the_field('kestavat_kaupungit2');?></div>
+      <div class="box-1" id="box11"><?php get_field('kestavat_kaupungit1');?></div>
+      <div class="box-2" id="box11"><?php get_field('kestavat_kaupungit2');?></div>
     </div>
     <div class="box">
       <div class="sdg-title">
         <h1><?php pll_e('SDG-12') ?></h1>
         <span class="circle-<?php echo get_field('circle_color_kopioi12')?>"></span>
       </div>
-      <div class="box-1" id="box12"><?php the_field('vastuullista_kuluttamista1');?></div>
-      <div class="box-2" id="box12"><?php the_field('vastuullista_kuluttamista2');?></div>
+      <div class="box-1" id="box12"><?php get_field('vastuullista_kuluttamista1');?></div>
+      <div class="box-2" id="box12"><?php get_field('vastuullista_kuluttamista2');?></div>
     </div>
     <div class="box">
       <div class="sdg-title">
         <h1><?php pll_e('SDG-13') ?></h1>
         <span class="circle-<?php echo get_field('circle_color_kopioi13')?>"></span>
       </div>
-      <div class="box-1" id="box13"><?php the_field('ilmastotekoja1');?></div>
-      <div class="box-2" id="box13"><?php the_field('ilmastotekoja2');?></div>
+      <div class="box-1" id="box13"><?php get_field('ilmastotekoja1');?></div>
+      <div class="box-2" id="box13"><?php get_field('ilmastotekoja2');?></div>
     </div>
     <div class="box">
       <div class="sdg-title">
         <h1><?php pll_e('SDG-14') ?></h1>
         <span class="circle-<?php echo get_field('circle_color_kopioi14')?>"></span>
       </div>
-      <div class="box-1" id="box14"><?php the_field('vedenalainen_elama1');?></div>
-      <div class="box-2" id="box14"><?php the_field('vedenalainen_elama2');?></div>
+      <div class="box-1" id="box14"><?php get_field('vedenalainen_elama1');?></div>
+      <div class="box-2" id="box14"><?php get_field('vedenalainen_elama2');?></div>
     </div>
     <div class="box">
       <div class="sdg-title">
         <h1><?php pll_e('SDG-15') ?></h1>
         <span class="circle-<?php echo get_field('circle_color_kopioi15')?>"></span>
       </div>
-      <div class="box-1" id="box15"><?php the_field('maanpaallinen_elama1');?></div>
-      <div class="box-2" id="box15"><?php the_field('maanpaallinen_elama2');?></div>
+      <div class="box-1" id="box15"><?php get_field('maanpaallinen_elama1');?></div>
+      <div class="box-2" id="box15"><?php get_field('maanpaallinen_elama2');?></div>
     </div>
     <div class="box">
       <div class="sdg-title">
         <h1><?php pll_e('SDG-16') ?></h1>
         <span class="circle-<?php echo get_field('circle_color_kopioi16')?>"></span>
       </div>
-      <div class="box-1" id="box16"><?php the_field('rauha_ja_oikeudenmukaisuus1');?></div>
-      <div class="box-2" id="box16"><?php the_field('rauha_ja_oikeudenmukaisuus2');?></div>    
+      <div class="box-1" id="box16"><?php get_field('rauha_ja_oikeudenmukaisuus1');?></div>
+      <div class="box-2" id="box16"><?php get_field('rauha_ja_oikeudenmukaisuus2');?></div>    
     </div>
     <div class="box">
       <div class="sdg-title">
         <h1><?php pll_e('SDG-17') ?></h1>
         <span class="circle-<?php echo get_field('circle_color_kopioi17')?>"></span>
       </div>
-      <div class="box-1" id="box17"><?php the_field('yhteistyo_ja_kumppanuus1');?></div>
-      <div class="box-2" id="box17"><?php the_field('yhteistyo_ja_kumppanuus2');?></div>
+      <div class="box-1" id="box17"><?php get_field('yhteistyo_ja_kumppanuus1');?></div>
+      <div class="box-2" id="box17"><?php get_field('yhteistyo_ja_kumppanuus2');?></div>
     </div>
   </div>
   
